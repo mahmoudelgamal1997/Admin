ca00b31e0cb3c9cfee402ff71e5d3cf3cb48194e
mahmoudelgamal1997 <mahmoudelgamal1997@gmail.com>
2018-01-19 15:27:56 +0200

    adding all codes to firebase

--- app/src/main/java/com/example2017/android/admin/shop_in_code.java
+++ app/src/main/java/com/example2017/android/admin/shop_in_code.java
@@ -1,7 +1,9 @@
 package com.example2017.android.admin;
 
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextPaint;
 import android.text.TextUtils;
 import android.view.View;
 import android.widget.EditText;
@@ -14,6 +16,15 @@ import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
 public class shop_in_code extends AppCompatActivity {
     DatabaseReference shop;
 
@@ -24,13 +35,10 @@ public class shop_in_code extends AppCompatActivity {
         setContentView(R.layout.activity_shop_in_code);
         Firebase.setAndroidContext(this);
 
-
         marketname=(EditText)findViewById(R.id.editText);
         value=(EditText)findViewById(R.id.editText2);
 
         shop= FirebaseDatabase.getInstance().getReference().child("codes");
-
-
 
     }
 
@@ -38,18 +46,18 @@ public class shop_in_code extends AppCompatActivity {
 
     public void add_all(String c,String k,String v)
     {
-
+        //this function add one value to all codes in the same time
         shop.child(c).child(k).setValue(v);
     }
 
 
-
+    //on click to button
     public void add(View v){
-
-        if(!TextUtils.isEmpty(marketname.getText().toString().trim())&&!TextUtils.isEmpty(value.getText().toString().trim())) {
-
-
-            shop.addChildEventListener(new ChildEventListener() {
+        //certain that fields not empty
+        if(!TextUtils.isEmpty(marketname.getText().toString().trim())  &&  !TextUtils.isEmpty(value.getText().toString().trim()))
+        {
+            shop.addChildEventListener(new ChildEventListener()
+            {
                 @Override
                 public void onChildAdded(DataSnapshot dataSnapshot, String s) {
                     //to get the key of first node
@@ -84,20 +92,11 @@ public class shop_in_code extends AppCompatActivity {
             if(marketname.getText().toString().trim().isEmpty())
             {
                 Toast.makeText(getApplicationContext(), "Market Name is Empty",Toast.LENGTH_LONG).show();
-
             }
             else
             {
                 Toast.makeText(getApplicationContext(), "Value Name is Empty",Toast.LENGTH_LONG).show();
-
             }
-
-
         }
-
     }
-
-
-
-
 }
